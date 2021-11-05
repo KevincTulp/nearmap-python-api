1200508e2fc9351987960c160e8f3bff662476d7
Geoff Taylor <geoffhtaylor3d@gmail.com>
2021-11-05 11:43:46 -0400

    Added Script for downloading imagery using polygons in GeoJSON file.

---
+++ examples/pipelines/Batch_Download_Ortho_from_parcel_boundaries.py
@@ -0,0 +1,94 @@
+from nearmap import NEARMAP
+from nearmap.auth import get_api_key
+from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+from glob import glob
+from shutil import rmtree
+from os.path import basename, dirname
+from os import rename
+
+
+def batch_download_ortho(api_key, in_geojson, fid_name, unique_id_name, geometry_name, out_folder, image_format="jpg",
+                         tertiary=None, since=None, until=None, mosaic=None, include=None,
+                         exclude=None, res=None):
+
+
+
+    def _restructure_data(in_folder, out_folder):
+        files = glob(f'{in_folder}/*')
+        for file in files:
+            unique_id = basename(dirname(file)).replace('f_', '')
+            file_name = basename(file)
+            new_file = f'{out_folder}/{file_name.replace("ortho_", "ortho_" + unique_id)}'
+            rename(file, new_file)
+        rmtree(in_folder)
+
+    def _file_exists(in_spreadsheet):
+        path = Path(in_spreadsheet)
+        assert path.is_file(), f"Error: input file does not exist: {in_spreadsheet}"
+
+    _file_exists(in_geojson)
+    file_extension = Path(in_geojson).suffix.lower()
+    supported_extensions = [".geojson"]
+    assert file_extension in supported_extensions, f"Error, input file extension not supported. " \
+                                                   f"Must be of {supported_extensions}"
+
+    gdf = gpd.read_file(in_geojson)
+
+
+    nearmap = NEARMAP(api_key)
+
+    p = Path(out_folder)
+    if p.exists():
+        rmtree(p)
+    p.mkdir(parents=True, exist_ok=False)
+
+    #df['download_status'] = ""
+    for index, row in gdf.iterrows():
+        print(f"processing {row[fid_name], row[unique_id_name]}")
+        try:
+            polygon = list(row.get(geometry_name).exterior.coords)
+            #polygon_formatted = [item for sublist in polygon for item in sublist]
+            #print(polygon_formatted)
+            out_file_basename = f"{out_folder}/{row[fid_name]}_{row[unique_id_name]}_"
+            nearmap.download_ortho(polygon, out_file_basename, image_format, tertiary, since, until, mosaic,
+                                   include, exclude, res)
+            _restructure_data(out_file_basename, out_folder)
+            #df.loc[index, ["download_status"]] = "Success"
+
+        except ModuleNotFoundError as e:
+            print(e)
+            exit()
+        except:
+            print(f"No Surveys Detected for {row[fid_name], row[unique_id_name]}'")
+            #df.loc[index, ["download_status"]] = "Fail"
+    #df.to_csv(f"{out_folder}\\results.csv")
+
+
+if __name__ == "__main__":
+    ###############
+    # User Inputs
+    #############
+
+    # Data Processing Specific Parameters
+
+    # Connect to the Nearmap API for Python
+    api_key = get_api_key()  # Paste or type your API Key here as a string
+
+    in_geojson = r'path_togeojson_file.geojson'  # Input Polygon GeoJson File
+
+    fid_name = 'My_Unique_Id_For_All_Features'  # Unique Feature ID for all geometries. Numbers can't exist > 1x in the dataset
+    unique_id_name = 'My_Unique_Parcel_ID'  # Parcel Lookup Code
+    geometry_name = 'geometry'  # Name for the geometry in geopandas
+
+    out_folder = r'output_folder_for_storing_imagery'
+    image_format = "jpg" # Member of "tif", "jpg", "jp2", "png", "cog"
+    tertiary = None
+    since = None
+    until = None
+    mosaic = None
+    include = None
+    exclude = None
+
+    batch_download_ortho(api_key, in_geojson, fid_name, unique_id_name, geometry_name, out_folder, image_format, tertiary, since, until, mosaic, include, exclude)
