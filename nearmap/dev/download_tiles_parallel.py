540a84e056f4753d0e55add8ac449bebeac50e03
Geoff Taylor <geoffhtaylor3d@gmail.com>
2021-11-21 21:24:10 -0500

    added tiletanic library to the environment.yml files

--- nearmap/dev/download_tiles_parallel.py
+++ nearmap/dev/download_tiles_parallel.py
@@ -8,6 +8,7 @@ from shutil import rmtree
 import geopandas as gpd
 import concurrent.futures
 from pathlib import Path
+from os.path import exists
 
 
 def download_tiles(in_params):
@@ -18,13 +19,13 @@ def download_tiles(in_params):
     _get_image(url=url, out_format=ext, out_image=path, rate_limit_mode="slow")
 
 
-def threaded_get_tiles(api_key, in_geojson, output_dir, threads=25):
+def threaded_get_tiles(api_key, in_geojson, output_dir, overwrite_images, threads=25):
 
     def _create_folder(folder):
         folder = Path(folder)
-        if folder.exists():
-            rmtree(folder)
-        folder.mkdir(parents=True, exist_ok=False)
+        #if folder.exists():
+        #    rmtree(folder)
+        folder.mkdir(parents=True, exist_ok=True)
         return folder
 
     assert Path(in_geojson).suffix.lower() in '.geojson', f'error: in_geojson not detected as geojson file: {in_geojson}'
@@ -39,6 +40,7 @@ def threaded_get_tiles(api_key, in_geojson, output_dir, threads=25):
 
     start = time.time()
     itr = 0
+    print("Begin loading Manifest")
     gdf = gpd.read_file(in_geojson)
     print(f"Preparing to download {len(gdf.index)} tiles")
 
@@ -47,15 +49,18 @@ def threaded_get_tiles(api_key, in_geojson, output_dir, threads=25):
         for index, row in tqdm(gdf.iterrows(), total=gdf.shape[0]):
             url = f'https://api.nearmap.com/tiles/v3/Vert/{row["zoom"]}/{row["x"]}/{row["y"]}.img?apikey={api_key}'
             path = f'{tiles_folder}\\{row["x"]}_{row["y"]}_{row["zoom"]}.img'
-            temp = dict()
-            temp['url'] = url
-            temp['path'] = path
-            temp['x'] = row["x"]
-            temp['y'] = row["y"]
-            temp['zoom'] = row["zoom"]
-            urls.append(temp)
-            itr += 1
-            jobs.append(executor.submit(download_tiles, temp))
+            if True in [Path(path.replace('.img', '.jpg')).is_file(), Path(path.replace('.img', '.png')).is_file()]:
+                print(f"skipping {path} | File Already Exists")
+            else:
+                temp = dict()
+                temp['url'] = url
+                temp['path'] = path
+                temp['x'] = row["x"]
+                temp['y'] = row["y"]
+                temp['zoom'] = row["zoom"]
+                urls.append(temp)
+                itr += 1
+                jobs.append(executor.submit(download_tiles, temp))
     print(f"Begin Downloading Tiles")
 
     results = []
@@ -74,8 +79,9 @@ if __name__ == "__main__":
 
     api_key = get_api_key()  # Edit api key in nearmap/api_key.py -or- type api key as string here
 
-    threads = 25
-    output_dir = os.path.join(os.path.abspath(''), 'miami_beach_data')
-    in_geojson = r'miami_beachOOO_1.geojson'
-
-    threaded_get_tiles(api_key, in_geojson, output_dir, threads)
+    threads = 20
+    #output_dir = os.path.join(os.path.abspath(''), 'miami_beach_data')
+    output_dir = r'D:\SNAP\LA\2255000_NewOrleans'
+    in_geojson = r'NewOrleans.geojson'
+    overwrite_images = False
+    threaded_get_tiles(api_key, in_geojson, output_dir, overwrite_images, threads)
