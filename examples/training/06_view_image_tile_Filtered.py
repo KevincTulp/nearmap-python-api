a30f8c86c876e9a4f478cfbc002ff1fdea9f0a2f
Geoff Taylor <geoffhtaylor3d@gmail.com>
2022-01-28 14:38:39 -0500

    Added Image Filtering Example using Pillow

--- examples/training/06_view_image_tile_Filtered.py
+++ examples/training/06_view_image_tile_Filtered.py
@@ -7,7 +7,7 @@ from PIL.ImageFilter import (
    EMBOSS, FIND_EDGES, SMOOTH, SMOOTH_MORE, SHARPEN
 )
 
-api_key = os.environ.get("NEARMAP_API_KEY")
+api_key = os.environ.get("API_KEY")
 nearmap = NEARMAP(api_key)
 
 ####################
@@ -23,35 +23,43 @@ filter_of_interest = CONTOUR  # Options: [BLUR, CONTOUR, DETAIL, EDGE_ENHANCE, E
 
 # Get Vert Image Tile as Bytes
 tileResourceType = "Vert"
-image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
-imBytes = Image.open(image_tile_bytes)
-imBytesFiltered = imBytes.filter(filter_of_interest)
-imBytesFiltered.show()
+vert_image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
+vert_imBytes = Image.open(vert_image_tile_bytes)
+vert_imBytesFiltered = vert_imBytes.filter(filter_of_interest)
+#vert_imBytesFiltered.show()
 
 # Get Vert Image Tile as Bytes
 tileResourceType = "North"
-image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
-imBytes = Image.open(image_tile_bytes)
-imBytesFiltered = imBytes.filter(filter_of_interest)
-imBytesFiltered.show()
+north_image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
+north_imBytes = Image.open(north_image_tile_bytes)
+north_imBytesFiltered = north_imBytes.filter(filter_of_interest)
+#north_imBytesFiltered.show()
 
 # Get Vert Image Tile as Bytes
 tileResourceType = "South"
-image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
-imBytes = Image.open(image_tile_bytes)
-imBytesFiltered = imBytes.filter(filter_of_interest)
-imBytesFiltered.show()
+south_image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
+south_imBytes = Image.open(south_image_tile_bytes)
+south_imBytesFiltered = south_imBytes.filter(filter_of_interest)
+#south_imBytesFiltered.show()
 
 # Get Vert Image Tile as Bytes
 tileResourceType = "East"
-image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
-imBytes = Image.open(image_tile_bytes)
-imBytesFiltered = imBytes.filter(filter_of_interest)
-imBytesFiltered.show()
+east_image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
+east_imBytes = Image.open(east_image_tile_bytes)
+east_imBytesFiltered = east_imBytes.filter(filter_of_interest)
+#east_imBytesFiltered.show()
 
 # Get Vert Image Tile as Bytes
 tileResourceType = "West"
-image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
-imBytes = Image.open(image_tile_bytes)
-imBytesFiltered = imBytes.filter(filter_of_interest)
-imBytesFiltered.show()
+west_image_tile_bytes = nearmap.tileV3(tileResourceType, z, x, y, format, "bytes", rate_limit_mode="slow")
+west_imBytes = Image.open(west_image_tile_bytes)
+west_imBytesFiltered = west_imBytes.filter(filter_of_interest)
+#west_imBytesFiltered.show()
+
+merged_image = Image.new('RGB', (5*vert_imBytes.size[0], vert_imBytes.size[1]), (250, 250, 250))
+merged_image.paste(vert_imBytesFiltered, (0, 0))
+merged_image.paste(north_imBytesFiltered, (vert_imBytes.size[0], 0))
+merged_image.paste(south_imBytesFiltered, (vert_imBytes.size[0]*2, 0))
+merged_image.paste(east_imBytesFiltered, (vert_imBytes.size[0]*3, 0))
+merged_image.paste(west_imBytesFiltered, (vert_imBytes.size[0]*4, 0))
+merged_image.show()
