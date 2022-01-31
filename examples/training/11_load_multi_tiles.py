cf51417dc65fce066026f81596ee60475ccff369
Geoff Taylor <geoffhtaylor3d@gmail.com>
2022-01-31 15:26:46 -0500

    Examples of loading Groups of tiles at once via matplotlib

---
+++ examples/training/11_load_multi_tiles.py
@@ -0,0 +1,48 @@
+from nearmap.geospatial.fileio import lat_lon_to_slippy_coords
+from nearmap import NEARMAP
+import os
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import ImageGrid
+from PIL import Image
+
+
+api_key = os.environ.get("NEARMAP_API_KEY")
+nearmap = NEARMAP(api_key)
+
+zoom = 19
+lat_deg = 40.76248769686459
+lon_deg = -111.89575293016202
+tileResourceType = "North"
+format = "img"
+num_rows = 4
+num_cols = 4
+axis_padding = 0.0
+enable_axis = False
+out_file = r'test.jpg'
+
+
+# Convert lat, lon and zoom to x,y,z
+x, y = lat_lon_to_slippy_coords(lat_deg, lon_deg, zoom)
+
+figure = plt.figure(figsize=(12., 12.))
+tile_grid = ImageGrid(figure,
+                      111,
+                      nrows_ncols=(num_rows, num_cols),
+                      axes_pad=axis_padding)
+
+grid_coords = [[row, col] for col in range(num_cols) for row in range(num_rows)]
+
+rotations = {"Vert": 0, "North": 0, "South": 180, "East": 270, "West": 90}
+
+for axis, (tile_x, tile_y) in zip(tile_grid, grid_coords):
+    image = Image.open(
+        nearmap.tileV3(tileResourceType, zoom, x+tile_x, y+tile_y, format,
+                       "bytes", rate_limit_mode="slow"))
+    axis.imshow(image.rotate(rotations.get(tileResourceType)))
+    if enable_axis:
+        axis.axis('on')
+    else:
+        axis.axis('off')
+plt.savefig(out_file, bbox_inches='tight', pad_inches=0)
+plt.show()
+
